1cf4a7ad741c017bcc4bff021ca1864e086db0b9
Sahar <sanaie.sahar@gmail.com>
2024-11-17 12:04:07 +0330

    data user

--- src/api.ts
+++ src/api.ts
@@ -2,5 +2,4 @@ import axios from 'axios';
 
 const BASE_URL = 'https://fakestoreapi.com';
 
-export const fetchProducts = () => axios.get(`${BASE_URL}/products`);
 export const fetchUser = () => axios.get(`${BASE_URL}/users/1`);
