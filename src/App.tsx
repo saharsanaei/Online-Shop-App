5924f8e2ff65ec533b6d8ce831a3f4f312f3c01e
Sahar <sanaie.sahar@gmail.com>
2024-11-17 14:38:06 +0330

    use tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import UserDetails from './components/UserDetails';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container mx-auto p-4">
       <UserDetails />
     </div>
   );
